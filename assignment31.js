fe4be72fb7144594db594156fd464f770e30fbf4
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:24:21 +0500

    Create assignment31.js

---
+++ assignment31.js
@@ -0,0 +1,19 @@
+"use strict";
+let users = ["Eric", "Daniyal", "FatimaK", "Maryam"];
+if (users.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    for (let user of users) {
+        if (user === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        }
+        // else {
+        //     console.log(`Hello, ${users} thank you for logging in again.`)
+        // }
+    }
+}
+users = [];
+if (users.length === 0) {
+    console.log('We need to find some users!');
+}
