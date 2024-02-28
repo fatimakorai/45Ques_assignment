b6a4a0ecb6582bf8db5b162cc1062c2a9fe49094
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:21:42 +0500

    Create assignment29.ts

---
+++ assignment29.ts
@@ -0,0 +1,11 @@
+let favorite_fruits :string[] = ['Apple', 'Pears', 'Banana'];
+
+if (favorite_fruits.includes('Banana')) {
+    console.log('You really like bananas!')
+}
+if (favorite_fruits.includes('Apple')) {
+    console.log('You really like apples!')
+}
+if (favorite_fruits.includes('Pears')) {
+    console.log('You really like pears!')
+}
