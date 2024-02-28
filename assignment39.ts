25fcd22076fd4d1bfe3878d148b6d9cb5e4703d1
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:33:01 +0500

    Create assignment39.ts

---
+++ assignment39.ts
@@ -0,0 +1,13 @@
+function city_country(city :string, country :string): string {
+        return '${city}, ${country}'
+} 
+
+let city1 = city_country('Karachi', 'Pakistan')
+let city2 = city_country('Istanbul', 'Turkey')
+let city3 = city_country('Toronto', 'Canada')
+
+console.log(city1)
+console.log(city2)
+console.log(city3)
+
+
