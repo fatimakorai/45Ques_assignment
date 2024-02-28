4e50802500d4c218cc1288eb47a48d2465fc9331
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:00:12 +0500

    Create assignment13.ts

---
+++ assignment13.ts
@@ -0,0 +1,6 @@
+let transportation :string[] = ['Cycle', 'Rikshaw', 'Car', 'Bike'];
+
+for (let i=0; i<transportation.length; i++) {
+
+    console.log('I would like to own a ' + transportation[i])
+}
