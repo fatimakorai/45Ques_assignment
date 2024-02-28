b980e95a92d428dc4029daa9d96841a02bacf096
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:30:19 +0500

    Create assignment36.js

---
+++ assignment36.js
@@ -0,0 +1,6 @@
+"use strict";
+function make_shirt(size, text) {
+    console.log(`You ordered a ${size} T-shirt that says ${text}`);
+}
+make_shirt("large", 'I like green color.');
+make_shirt("small", 'I like red color.');
