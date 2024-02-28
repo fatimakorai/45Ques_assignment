f5efea56083a3d424817860b517e3c4ab7f77595
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:31:02 +0500

    Create assignment37.ts

---
+++ assignment37.ts
@@ -0,0 +1,5 @@
+function make_Shirts (size: string = 'large', text: string = ' I love TypeScript.'): void {
+    console.log(`You have ordered a ${size} shirt that says ${text}`)
+}
+
+make_Shirts ('small', 'I need a big shirt for myself.')
