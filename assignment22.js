25455c8659e3bf804fada0863b5d4e9f74d0858e
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:12:08 +0500

    Create assignment22.js

---
+++ assignment22.js
@@ -0,0 +1,8 @@
+"use strict";
+//Creating array
+let firstArray = ['Finances', 'Accounting', 'Marketing'];
+//Accessing array by index
+console.log(firstArray[3]);
+//Make sure you correct the error before closing the program.
+//Error Correction:
+console.log(firstArray);
