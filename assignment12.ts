1ad1d231ab05d55a8975b8a021a778db74098499
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 03:59:10 +0500

    Create assignment12.ts

---
+++ assignment12.ts
@@ -0,0 +1,8 @@
+let friends :string[] = ['Aliya', 'Amna', 'Mariyam', 'Fatima'];
+
+const message :string = 'We will learn coding: '
+
+for (let i=0; i<friends.length; i++)
+ {
+    console.log(message + friends[i]);
+} 
