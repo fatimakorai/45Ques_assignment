1e549f789c6aa3fdbecc1c100ac400b2596169f9
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:11:18 +0500

    Create assignment21.ts

---
+++ assignment21.ts
@@ -0,0 +1,38 @@
+// Human datatype
+
+interface typescript_Obj {
+        age: number;
+        name: string;
+        nationality: string;
+        student: Boolean;
+}
+
+// Human Object
+let typescript_Obj :typescript_Obj = {
+        age : 18,
+        name : 'FatimaK',
+        nationality: 'Pakistani',
+        student: true
+
+}
+console.log(typescript_Obj);
+
+// Dress datatype
+
+interface dress_Obj {
+    dress: string;
+    color: string;
+    female: boolean;
+    quantity: number;
+}
+
+// Dress Object
+
+let dress_Obj :dress_Obj = {
+    dress: 'Western Skirt',
+    color: 'Black',
+    female: true,
+    quantity: 2,
+}
+
+console.log(dress_Obj);
