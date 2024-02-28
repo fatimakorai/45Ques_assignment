e38350e815a30596980b633b4bbe4fc008a64a88
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 03:48:36 +0500

    Create assignment03.ts

---
+++ assignment03.ts
@@ -0,0 +1,17 @@
+personName = prompt('What is your name?') || '';
+
+let lowercase :string = personName.toLowerCase();
+let uppercase :string = personName.toUpperCase();
+let titlecase :string = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).
+toLowerCase()).join(' ');
+
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, here is your name in:
+    Lowercase: ${lowercase}
+    Uppercase: ${uppercase}
+    Titlecase: ${titlecase}`)
+} 
+
+else {
+    alert('Please fill your name.')
+} 
