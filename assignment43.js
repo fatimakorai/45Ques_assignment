1d3d1ac1ec3ef74d6653220a05b605b1f296936f
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:37:46 +0500

    Create assignment43.js

---
+++ assignment43.js
@@ -0,0 +1,26 @@
+"use strict";
+let magicians = ['John', 'Tom', 'Winslet'];
+function show_magicians(magicians) {
+    for (const magician of magicians) {
+        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+    }
+}
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] += " the Great";
+    }
+}
+make_great(magicians);
+show_magicians(magicians);
+// 43 Started
+function make_great2(magicians) {
+    const greatMagicians = [];
+    for (let i = 0; i < magicians.length; i++) {
+        greatMagicians.push(magicians[i] + ' the Great');
+    }
+    return greatMagicians;
+}
+const magicians3 = ['John', 'Tom', 'Winslet'];
+const greatMagicians2 = make_great2(magicians);
+show_magicians(magicians3);
+show_magicians(greatMagicians2);
