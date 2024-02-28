6939dcde557066374e115bb88552ed698a25b4cc
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:10:38 +0500

    Create assignment21.js

---
+++ assignment21.js
@@ -0,0 +1,18 @@
+"use strict";
+// Human datatype
+// Human Object
+let typescript_Obj = {
+    age: 18,
+    name: 'FatimaK',
+    nationality: 'Pakistani',
+    student: true
+};
+console.log(typescript_Obj);
+// Dress Object
+let dress_Obj = {
+    dress: 'Western Skirt',
+    color: 'Black',
+    female: true,
+    quantity: 2,
+};
+console.log(dress_Obj);
