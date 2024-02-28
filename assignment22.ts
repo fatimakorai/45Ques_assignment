c73f9e406194a67b27e5047b26383f259bb700bd
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:12:38 +0500

    Create assignment22.ts

---
+++ assignment22.ts
@@ -0,0 +1,13 @@
+//Creating array
+
+let firstArray :string[] = ['Finances', 'Accounting', 'Marketing'];
+
+//Accessing array by index
+
+console.log(firstArray[3]);
+
+//Make sure you correct the error before closing the program.
+
+//Error Correction:
+
+console.log(firstArray);
