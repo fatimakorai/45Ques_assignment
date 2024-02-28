132f5d7d8160cbc9a0955323690511995d107818
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:28:34 +0500

    Create assignment35.ts

---
+++ assignment35.ts
@@ -0,0 +1,11 @@
+let animals :string[] = ['Tiger', 'Lion', 'Cat'];
+
+for (let animal of animals) {
+    console.log(animal)
+}
+
+for (let animal of animals) {
+    console.log(` A ${animal}  has a tail.`)
+}
+
+console.log('\n\n These animals are very brave, and any of these animals would make a great pet!')
