769a6c11fd1f54aa111a8cdf84de22e647b5d0de
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:31:31 +0500

    Create assignment37.js

---
+++ assignment37.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_Shirts(size = 'large', text = ' I love TypeScript.') {
+    console.log(`You have ordered a ${size} shirt that says ${text}`);
+}
+make_Shirts('small', 'I need a big shirt for myself.');
