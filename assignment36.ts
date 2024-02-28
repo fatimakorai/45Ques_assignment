8ab9c20e1d2ddca02a245c59da27361d20f7341b
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:29:43 +0500

    Create assignment36.ts

---
+++ assignment36.ts
@@ -0,0 +1,7 @@
+function make_shirt(size: string, text: string): void {
+    console.log(`You ordered a ${size} T-shirt that says ${text}`)
+
+} 
+
+make_shirt("large", 'I like green color.');
+make_shirt("small", 'I like red color.');
