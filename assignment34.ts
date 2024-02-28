fa1a0f154c6211cf14dae53b45138eb105d92482
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:27:27 +0500

    Create assignment34.ts

---
+++ assignment34.ts
@@ -0,0 +1,7 @@
+let fav_pizzas :string[] = ['pepperoni', 'Fajita', 'Malai Boti'];
+
+for (let pizza of fav_pizzas) {
+    console.log(`I really like ${pizza} pizza!`)
+}
+
+console.log('\nI really love pizza!')
