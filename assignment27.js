052d68e72f8ce0a3e22367ff413198e7cfa4bb37
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:19:43 +0500

    Create assignment27.js

---
+++ assignment27.js
@@ -0,0 +1,14 @@
+"use strict";
+let alien_color = 'green';
+// Create a variable called alien_color and assign it a value of 'green'
+if (alien_color === 'green') {
+    console.log('Player earned 5 Points.');
+}
+alien_color = 'yellow';
+if (alien_color === 'yellow') {
+    console.log('Player earned 10 points.');
+}
+alien_color = 'red';
+if (alien_color === 'red') {
+    console.log('Player earned 15 points.');
+}
