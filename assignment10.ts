119b502eb29cede310f96b1ee820cd6663cab582
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 03:56:22 +0500

    Create assignment10.ts

---
+++ assignment10.ts
@@ -0,0 +1,10 @@
+// program written by FatimaK
+// current date 27/02/2024
+
+//This program will write Salam Coders.
+
+console.log('Salam Coders');
+
+//This program will write Hello I am learning typescript.
+
+console.log('Hello I am learning typescript.');
