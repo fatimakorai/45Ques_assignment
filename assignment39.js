839ad56fb010c6a0fe8f2d141782282178e0ee4d
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:33:23 +0500

    Create assignment39.js

---
+++ assignment39.js
@@ -0,0 +1,10 @@
+"use strict";
+function city_country(city, country) {
+    return '${city}, ${country}';
+}
+let city1 = city_country('Karachi', 'Pakistan');
+let city2 = city_country('Istanbul', 'Turkey');
+let city3 = city_country('Toronto', 'Canada');
+console.log(city1);
+console.log(city2);
+console.log(city3);
