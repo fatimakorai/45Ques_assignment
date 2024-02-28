18b0d7e70fc2c6b5fab89101f5963c60a2ee696b
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 03:46:35 +0500

    Create assignment02.ts

---
+++ assignment02.ts
@@ -0,0 +1,7 @@
+let humanName :string = "Mariyam";
+
+humanName = prompt("What is your name?") || '';
+
+if(humanName !== null && humanName !== '') {
+    alert(`Hello ${humanName}, would you like to learn some Python today?`)
+}
