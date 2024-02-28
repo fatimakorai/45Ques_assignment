e69573e5162f892c32e467bb44118bb2bca86334
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:18:46 +0500

    Create assignment27.ts

---
+++ assignment27.ts
@@ -0,0 +1,20 @@
+let alien_color :string = 'green';
+
+// Create a variable called alien_color and assign it a value of 'green'
+
+if (alien_color === 'green') {
+    console.log('Player earned 5 Points.');
+}
+
+
+alien_color = 'yellow';
+
+if (alien_color === 'yellow') {
+    console.log('Player earned 10 points.');
+}
+
+alien_color = 'red';
+
+if (alien_color === 'red') {
+    console.log('Player earned 15 points.');
+}
