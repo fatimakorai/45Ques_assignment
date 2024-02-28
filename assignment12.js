6d0693bb0c255bee693e0691e9bfb60386a5a56f
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 03:58:41 +0500

    Create assignment12.js

---
+++ assignment12.js
@@ -0,0 +1,6 @@
+"use strict";
+let friends = ['Aliya', 'Amna', 'Mariyam', 'Fatima'];
+const message = 'We will learn coding: ';
+for (let i = 0; i < friends.length; i++) {
+    console.log(message + friends[i]);
+}
