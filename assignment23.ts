26f6eb561aba54b64143c36e132da8c678f2d97c
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:13:31 +0500

    Create assignment23.ts

---
+++ assignment23.ts
@@ -0,0 +1,45 @@
+//Write a series of conditional tests.
+
+//Creating car variable
+
+let car  :string = 'subaru';
+
+//Test no 1
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
+//Test no 2
+console.log("Is car === 'subaru'? I predict True.")
+console.log(car === 'subaru')
+
+//Test no 3
+console.log("Is car != 'subaru'? I predict True.")
+console.log(car != 'subaru')
+
+//Test no 4
+console.log("Is car !== 'subaru'? I predict True.")
+console.log(car !== 'subaru')
+
+//Test no 5
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
+//Test no 6
+console.log("Is car. lowercase() == 'subaru'? I predict True.")
+console.log(car.toLowerCase() == 'subaru')
+
+//Test no 7
+console.log("Is car. uppercase() == 'SUBARU'? I predict True.")
+console.log(car.toUpperCase() == 'SUBARU')
+
+//Test no 8
+console.log("Is car. uppercase() == 'subaru'? I predict True.")
+console.log(car.toUpperCase() == 'subaru')
+
+//Test no 9
+console.log("Is car == 'subarU'? I predict True.")
+console.log(car  == 'subarU')
+
+//Test no 10
+console.log("Is car == 'suBaru'? I predict True.")
+console.log(car  == 'suBaru')
