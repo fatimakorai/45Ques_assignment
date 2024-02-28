c0cc1503832dd8f0fe1ec1e5148c77f877eb6201
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:14:40 +0500

    Create assignment24.ts

---
+++ assignment24.ts
@@ -0,0 +1,36 @@
+//equality and inequality test
+//more_conditional_tests.js
+//Test 1
+console.log('Equality test with strings: ', "Apple" === "Apple");
+//Test 2
+console.log('Inequality test with strings: ', "Layer" != "Layer");
+//Test 3
+console.log('Tests using the lower case functions: ', "strawberry" == "strawberry");
+//Test 4
+console.log('Tests using the lower case functions: ', ("strawberry" as string) == "apples");
+//Test 5
+console.log('Tests using the lower case functions: ', ("strawberry" as string) != "apples");
+//Test 6
+console.log('Tests using the lower case functions: ', ("strawberry" as string) != "strawberry");
+//Test 7
+console.log('Equality Test with numbers: ',  10 == 10);
+//Test 8
+console.log('Inequality Test with numbers: ',  10 as number == 11);
+//Test 9
+console.log('Inequality Test with numbers: ',  20 == 20);
+//Test 10
+console.log('Equality Test with numbers: ',  20 > 20);
+//Test 11
+console.log('Equality Test with numbers: ',  20 < 30);
+//Test 12
+console.log('Equality Test with numbers: ',  20 < 10);
+//Test 13
+console.log('Equality Test with numbers: ',  20  || 20);
+//Test 14
+console.log('Inequality Test with numbers: ',  10  || 20);
+//Test 15
+let array_testing: number[] = [1, 2, 3, 4, 5];
+console.log('Equality Test with numbers: ', array_testing.includes(1)); 
+//Test 16
+let array_testing1: number[] = [1, 2, 3, 4, 5];
+console.log('Inequality Test with numbers: ',array_testing1.includes(9)); 
