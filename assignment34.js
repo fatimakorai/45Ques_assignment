49d5bbd6b5b46b863a5fec3f09a6eaec17db8941
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:27:55 +0500

    Create assignment34.js

---
+++ assignment34.js
@@ -0,0 +1,6 @@
+"use strict";
+let fav_pizzas = ['pepperoni', 'Fajita', 'Malai Boti'];
+for (let pizza of fav_pizzas) {
+    console.log(`I really like ${pizza} pizza!`);
+}
+console.log('\nI really love pizza!');
