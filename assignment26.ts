c28e962669a3bbb89540968059e4b4619f19f27d
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:17:44 +0500

    Create assignment26.ts

---
+++ assignment26.ts
@@ -0,0 +1,9 @@
+let alien_color :string = 'red';
+
+
+if (alien_color === 'green') {
+    console.log('Player just earned 5 points for shooting the alien.');
+}
+else {
+    console.log('Player just earned 10 Points.');
+}
