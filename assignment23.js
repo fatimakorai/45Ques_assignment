c8cf1d20c59af34db2853e3fa75503b9bc441936
Fatima Murtaza <111973987+fatimakorai@users.noreply.github.com>
2024-02-29 04:13:57 +0500

    Create assignment23.js

---
+++ assignment23.js
@@ -0,0 +1,16 @@
+"use strict";
+//Write a series of conditional tests.
+//Creating car variable
+let car = 'subaru';
+//Test no 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//Test no 2
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+//Test no 3
+console.log("Is car != 'subaru'? I predict True.");
+console.log(car != 'subaru');
+//Test no 4
+console.log("Is car !== 'subaru'? I predict True.");
+console.log(car !== 'subaru');
